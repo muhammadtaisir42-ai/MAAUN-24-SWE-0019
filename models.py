6dab5d8b1dccbbb50dee275cd8305f48922e976e
muhammadtaisir42-ai <muhammadtaisir42@gmail.com>
2026-03-18 16:30:28 +0100

    Implemented queue data structure for managing questions

--- models.py
+++ models.py
@@ -5,7 +5,8 @@ class Question:
         self.text = text
         self.options = options
         self.answer = answer
-
+def get_next_question(self):
+    return self.questions.pop(0)
 
     def is_correct(self, choice):
         return choice == self.answer
